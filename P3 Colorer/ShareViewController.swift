ad6d21e14fccf2b1208a1adc5195e5776b04470e
myo3@cornell.edu <Monica@Monicas-MacBook-Pro.local>
2016-01-05 10:22:21 -0500

    fix glitch: save button now indicates to users that save was successful

--- P3 Colorer/ShareViewController.swift
+++ P3 Colorer/ShareViewController.swift
@@ -40,30 +40,29 @@ class ShareViewController: UIViewController {
         let buttonSize = ((view.bounds.height - preview.frame.maxY) - space*3)/2
         
         let returnButton = UIButton(frame: CGRectMake(0, 0, buttonSize, buttonSize))
-        returnButton.setBackgroundImage(UIImage(named: "returnIcon"), forState: .Normal)
+        returnButton.setImage(UIImage(named: "returnIcon"), forState: .Normal)
         returnButton.center = CGPoint(x: view.bounds.width/2, y: view.bounds.height - returnButton.bounds.height/2 - space)
         returnButton.addTarget(self, action: "returnDown:", forControlEvents: .TouchUpInside)
         self.view.addSubview(returnButton)
         
         //create save button
         let saveButton = UIButton(frame: CGRectMake(0, preview.frame.maxY + space, buttonSize, buttonSize))
-        saveButton.setBackgroundImage(UIImage(named: "saveIcon"), forState: .Normal)
-        saveButton.setBackgroundImage(UIImage(named: "doneIcon"), forState: .Disabled)
+        saveButton.setImage(UIImage(named: "saveIcon"), forState: .Normal)
+        saveButton.setImage(UIImage(named: "doneIcon"), forState: .Disabled)
         saveButton.center.x = returnButton.center.x
         saveButton.addTarget(self, action: "save:", forControlEvents: .TouchUpInside)
-        saveButton.addTarget(self, action: "canSaveAgain:", forControlEvents: .TouchUpInside)
         self.view.addSubview(saveButton)
         
         //create facebook button
         let centerX = view.bounds.width/6
         let facebookButton  = UIButton(frame: CGRectMake(saveButton.frame.minX - space - buttonSize, saveButton.frame.minY, buttonSize, buttonSize))
-        facebookButton.setBackgroundImage(UIImage(named: "facebookIcon"), forState: .Normal)
+        facebookButton.setImage(UIImage(named: "facebookIcon"), forState: .Normal)
         facebookButton.addTarget(self, action: "postFacebook:", forControlEvents: .TouchUpInside)
         self.view.addSubview(facebookButton)
         
         //create twitter button
         let twitterButton  = UIButton(frame: CGRectMake(saveButton.frame.maxX + space, saveButton.frame.minY, buttonSize, buttonSize))
-        twitterButton.setBackgroundImage(UIImage(named: "twitterIcon"), forState: .Normal)
+        twitterButton.setImage(UIImage(named: "twitterIcon"), forState: .Normal)
         twitterButton.addTarget(self, action: "postTwitter:", forControlEvents: .TouchUpInside)
         self.view.addSubview(twitterButton)
     }
@@ -90,7 +89,7 @@ class ShareViewController: UIViewController {
     func save(sender: UIButton){
         UIImageWriteToSavedPhotosAlbum(masterpiece, nil, nil, nil)
         sender.enabled = false
-        self.performSelector("canSaveAgain:", withObject: sender, afterDelay: 1.2)
+        self.performSelector("canSaveAgain:", withObject: sender, afterDelay: 1.0)
     }
     
     func canSaveAgain(sender: UIButton){
